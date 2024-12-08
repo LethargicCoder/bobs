5daba0877d81fe2f2f40595695c5571c413c2fa7
Eric Cestnick <eric.cestnick@gmail.com>
2024-12-07 20:12:23 -0400

    init setup

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # bobs
 test theme / template library
+
+Add Package Service Provider
+To use this package in your Laravel application, add the service provider to the config/app.php file under the providers array:
+
+'providers' => [
+    // Other service providers
+    LethargicCoder\Bobs\BobsServiceProvider::class,
+],
---
+++ src/BobsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+namespace LethargicCoder\Bobs;
+
+use Illuminate\Support\ServiceProvider;
+
+class BobsServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // Publish your package resources, such as config files, views, etc.
+        // $this->publishes([
+        //     __DIR__ . '/../config/bobs.php' => config_path('bobs.php'),
+        // ]);
+
+        // Load routes, migrations, and views if applicable
+        // $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'bobs');
+        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations'); 
+    }
+
+    public function register()
+    {
+        // Register any application services or bindings.
+        // $this->mergeConfigFrom(
+        //     __DIR__.'/../config/bobs.php', 'bobs'
+        // );
+    }
+}
